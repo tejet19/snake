6d62a32a57b4c2d1711ee527e40add95f53b61ce
soumak77 <bsoumakian@gmail.com>
2015-04-02 14:29:30 -0700

    Updates to allow level up

--- src/js/game.js
+++ src/js/game.js
@@ -5,43 +5,76 @@
     this.snake = null;
     this.coin = null;
     this.map = null;
-    this.layer = null;
     this.livesText = null;
     this.scoreText = null;
-    this.introText = null;
+    this.mainText = null;
     this.lives = 3;
     this.score = 0;
     this.gameStarted = false;
     this.cursors = null;
     this.lastButton = null;
-    this.level = 1;
+
+    this.scoreToLevelUp = 30;
+
+    this.startingLevel = 1;
+    this.currentLevel = null;
+    this.levels = [
+      {
+        level: 1,
+        layer: null,
+        layerName: 'Tile Layer 1',
+        collisionTiles: [414, 415, 451, 452],
+        scoreToBeat: 10,
+        startingPos: {
+          x: 0,
+          y: 0
+        },
+        startingDir: 'down'
+      },
+      {
+        level: 2,
+        layer: null,
+        layerName: 'Tile Layer 2',
+        collisionTiles: [6, 7, 9, 136],
+        scoreToBeat: 20,
+        startingPos: {
+          x: 0,
+          y: 0
+        },
+        startingDir: 'right'
+      },
+      {
+        level: 3,
+        layer: null,
+        layerName: 'Tile Layer 3',
+        collisionTiles: [6, 7],
+        scoreToBeat: 30,
+        startingPos: {
+          x: 0,
+          y: 0
+        },
+        startingDir: 'right'
+      }
+    ];
   }
 
   Game.prototype = {
 
     create: function () {
+      var i;
+
       this.physics.startSystem(Phaser.Physics.ARCADE);
 
-      this.stage.backgroundColor = '#000';
+      this.stage.backgroundColor = '#e0e0e0';
       this.map = this.add.tilemap('mario');
       this.map.addTilesetImage('Mario_Tiles', 'tiles');
 
-      if (this.level === 1) {
-        // Setup for Tile Layer 1
-        this.layer = this.map.createLayer('Tile Layer 1');
-        this.map.setCollisionBetween(414, 415);
-        this.map.setCollisionBetween(451, 452);
-      } else if (this.level === 2) {
-        // Setup for Tile Layer 2
-        this.layer = this.map.createLayer('Tile Layer 2');
-        this.map.setCollision([6, 7, 136], true, this.layer);
-      } else if (this.level === 3) {
-        // Setup for Tile Layer 3
-        this.layer = this.map.createLayer('Tile Layer 3');
-        this.map.setCollision([6], true, this.layer);
+      for(i = 0; i < this.levels.length; i++) {
+        // load layers
+        this.levels[i].layer = this.map.createLayer(this.levels[i].layerName);
+        this.map.setCollision(this.levels[i].collisionTiles, true, this.levels[i].layer);
+        this.levels[i].layer.visible = false;
       }
-
-      this.layer.resizeWorld();
 
       this.cursors = this.input.keyboard.createCursorKeys();
 
@@ -54,7 +87,6 @@
       /* Coin */
       this.coin = this.add.sprite(0, 0, 'sprites', 'gold-coin1');
       this.coin.animations.add('spin', ['gold-coin1', 'gold-coin2', 'gold-coin3', 'gold-coin4'], 10, true, false);
-      this.coin.kill();
 
       /* Text */
       this.scoreText = this.add.text(32, this.world.height - 50, 'score: 0', { font: '20px Arial', fill: '#ffffff', align: 'left' });
@@ -62,15 +94,17 @@
       this.livesText = this.add.text(this.world.width - 32, this.world.height - 50, 'lives: 3', { font: '20px Arial', fill: '#ffffff', align: 'left' });
       this.livesText.anchor.setTo(1, 0);
 
-      this.introText = this.add.text(this.world.centerX, this.world.centerY, '- click to start -', { font: '40px Arial', fill: '#ffffff', align: 'center' });
-      this.introText.anchor.setTo(0.5, 0.5);
+      this.mainText = this.add.text(this.world.centerX, this.world.centerY, '- click to start -', { font: '40px Arial', fill: '#ffffff', align: 'center' });
+      this.mainText.anchor.setTo(0.5, 0.5);
 
-      this.input.onDown.add(this.startGame, this);
+      this.currentLevel = this.levels[this.startingLevel - 1];
+      this.prepareLevel();
+      this.input.onDown.add(this.startLevel, this);
     },
 
     update: function () {
       if (this.gameStarted) {
-        this.physics.arcade.collide(this.snake, this.layer, this.boundsHit, null, this);
+        this.physics.arcade.collide(this.snake, this.currentLevel.layer, this.boundsHit, null, this);
         if (this.isSnakeOverlappingCoin()) {
           this.collectCoin();
         }
@@ -127,7 +161,95 @@
             this.lastButton = 'down';
           }
         }
+
+        // check for level up
+        if (this.checkIfLevelUp()) {
+          this.levelUp();
+        }
       }
+    },
+
+    checkIfLevelUp: function() {
+      return this.score >= this.currentLevel.scoreToBeat;
+    },
+
+    levelUp: function() {
+      // hide previous level
+      this.currentLevel.layer.visible = false;
+
+      // change current level to next level
+      if (this.levels.length === this.currentLevel.level) {
+        // we reached the last level
+        this.gameWon();
+      } else {
+        // at least one level remaining
+        this.currentLevel = this.levels[(this.currentLevel.level + 1) - 1];
+
+        this.mainText.text = '- level ' + this.currentLevel.level + ' -';
+        this.mainText.visible = true;
+
+        this.gameStarted = false;
+        this.prepareLevel();
+      }
+    },
+
+    prepareLevel: function() {
+      this.mainText.text = '- click to start -';
+      this.mainText.visible = true;
+
+      this.coin.kill();
+
+      // set snake starting position
+      this.snake.x = this.currentLevel.startingPos.x;
+      this.snake.y = this.currentLevel.startingPos.y;
+
+      // stop snake from moving
+      this.snake.body.velocity.x = 0;
+      this.snake.body.velocity.y = 0;
+
+      // set layer for level
+      this.currentLevel.layer.visible = true;
+      this.currentLevel.layer.resizeWorld();
+    },
+
+    startLevel: function () {
+      if (!this.gameStarted) {
+        // hide text
+        this.mainText.visible = false;
+        this.livesText.text = 'lives: ' + this.lives;
+        this.scoreText.text = 'score: ' + this.score;
+
+        // reset coin
+        this.coin.revive();
+        this.moveCoinToRandomPos();
+        this.coin.animations.play('spin');
+
+        // reset button queued
+        this.lastButton = null;
+
+        // start game
+        this.gameStarted = true;
+
+        // start moving snake
+        this.snake.revive();
+        this.changeSnakeDir(this.currentLevel.startingDir);
+      }
+    },
+
+    gameOver: function() {
+      this.mainText.text = 'Game Over!';
+      this.mainText.visible = true;
+      this.lives = 3;
+
+      this.prepareLevel();
+    },
+
+    gameWon: function() {
+      this.mainText.text = 'You Win!';
+      this.mainText.visible = true;
+      this.lives = 3;
+
+      this.prepareLevel();
     },
 
     isSwiping: function(dir) {
@@ -203,45 +325,15 @@
       return movingDir === dir;
     },
 
-    startGame: function () {
-      if (!this.gameStarted) {
-        // hide text
-        this.introText.visible = false;
-        this.livesText.text = 'lives: ' + this.lives;
-        this.scoreText.text = 'score: ' + this.score;
-
-        // reset coin
-        this.coin.revive();
-        this.moveCoinToRandomPos();
-        this.coin.animations.play('spin');
-
-        // reset button queued
-        this.lastButton = null;
-
-        // start game
-        this.gameStarted = true;
-
-        // start moving snake
-        this.changeSnakeDir('down');
-      }
-    },
-
     boundsHit: function () {
       if (this.gameStarted) {
-        // reset snake
-        this.snake.x = 0;
-        this.snake.y = 0;
-        this.snake.body.velocity.x = 0;
-        this.snake.body.velocity.y = 0;
-
-        this.coin.kill();
         this.lives--;
         this.livesText.text = 'lives: ' + this.lives;
         this.gameStarted = false;
         if (this.lives === 0) {
           this.gameOver();
         } else {
-          this.resetLevel();
+          this.prepareLevel();
         }
       }
     },
@@ -261,19 +353,8 @@
         this.coin.x = this.rnd.between(0, 25) * 16;
         this.coin.y = this.rnd.between(0, 25) * 16;
         snakeCoinOverlap = this.physics.arcade.overlap(this.snake, this.coin);
-        layerCoinOverlap = this.physics.arcade.overlap(this.layer, this.coin);
+        layerCoinOverlap = this.physics.arcade.overlap(this.currentLevel.layer, this.coin);
       }
-    },
-
-    resetLevel: function() {
-      this.introText.text = '- click to start -';
-      this.introText.visible = true;
-    },
-
-    gameOver: function() {
-      this.introText.text = 'Game Over!';
-      this.introText.visible = true;
-      this.lives = 3;
     },
 
     isSnakeOverlappingCoin: function() {
